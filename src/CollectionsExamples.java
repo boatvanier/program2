8bb5523b8a44465f682b6762609173e69854cb45
boatvanier <csvaniercourse@gmail.com>
2026-01-08 11:25:04 -0500

    more comments

--- src/CollectionsExamples.java
+++ src/CollectionsExamples.java
@@ -4,6 +4,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CollectionsExamples {
+    // NO GOOD: List<String> list = new List<>();
+
+
     static void arrayListExample(){
         List<String> names = new ArrayList<>();
 
@@ -27,17 +30,20 @@ public class CollectionsExamples {
 
         // remove by index
         names.remove(1);
+        System.out.println(names);
 
         // remove by value
         names.remove("Alice"); // removes first occurrence
+        System.out.println(names);
 
         // size
         System.out.println("Size: " + names.size());
 
         // contains
-        System.out.println(names.contains("Bob"));
+        System.out.println(names.contains("Alice"));
 
         // iterate
+        names.add("Bob");
         for (String name : names) {
             System.out.println(name);
         }
@@ -58,8 +64,10 @@ public class CollectionsExamples {
 
         // remove by index
         numbers.remove(2);
-
+        // [10 ,15 ,30]
+        // update element by index
         numbers.set(1, 26);
+        // [10, 26, 30]
 
         // iterate using iterator
         Iterator<Integer> it = numbers.iterator();
